5e29f8d6acb92915fb8ce8bfbe9a0b611751a132
Jayesh Devre <84898706+JayeshDevre@users.noreply.github.com>
2025-01-20 13:38:00 -0700

    Time: 0 ms (100%), Space: 41 MB (33.93%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,10 +1,8 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-       for(int i=0;i<31;i++){
-        if(n==Math.pow(4,i)){
-            return true;
+        if (n <= 0) {
+            return false;
         }
-       }
-       return false;
+        return Math.log(n) / Math.log(4) % 1 == 0;        
     }
 }
