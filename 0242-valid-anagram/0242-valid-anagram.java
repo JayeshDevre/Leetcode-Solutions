ce720546aac9ac39fbfa0cc9df160b40417a8341
Jayesh Devre <84898706+JayeshDevre@users.noreply.github.com>
2025-02-28 16:16:57 -0700

    Time: 24 ms (8.37%), Space: 43.5 MB (66.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,12 +3,14 @@ class Solution {
         HashMap<Character, Integer> map=new HashMap<>();
 
 
+if(s.length()!=t.length()){
+    return false;
+}
         for(int i=0;i<s.length();i++){
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i),0)+1);
-        }
-        for(int i=0;i<t.length();i++){
             map.put(t.charAt(i), map.getOrDefault(t.charAt(i),0)-1);
         }
+        
 
         for(int values: map.values()){
             if(values!=0){
