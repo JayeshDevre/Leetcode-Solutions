3d095263076f79d4cbbfeb2f61f7a412ee4cbd8a
Jayesh Devre <84898706+JayeshDevre@users.noreply.github.com>
2025-03-01 22:15:51 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,21 +1,11 @@
 class Solution {
     public int climbStairs(int n) {
-        int result=0;
+       int result=0;
 
-        if(n<=2){
-            return n;
-        }
+       if(n<=2){
+        return n;
+       }
 
-        int prev=1;
-        int latest=1;
-
-
-        for(int i=2;i<=n;i++){
-            int temp=latest;
-            latest=prev+latest;
-            prev=temp;
-        }
-
-        return latest;
+       return climbStairs(n-1)+climbStairs(n-2);
     }
 }
