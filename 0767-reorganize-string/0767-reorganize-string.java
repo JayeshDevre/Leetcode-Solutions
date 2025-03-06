8de658bea6bbae4f07b401c0e04b2f41f7f55238
Jayesh Devre <84898706+JayeshDevre@users.noreply.github.com>
2025-03-06 12:10:46 -0700

    Time: 1 ms (92.8%), Space: 41.3 MB (96.73%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,17 +1,18 @@
 class Solution {
     public String reorganizeString(String s) {
-
         int [] hash= new int[26];
-        // Count the frequency of each character
-        for(int i=0;i<s.length();i++) {
+
+        // count the frequency of each character in string
+
+        for(int i=0;i<s.length();i++){
             hash[s.charAt(i)-'a']++;
-        }  
+        }
 
-
+        // variable max and letter to store occurences and letter 
         int max=0;
         int letter=0;
-        
 
+        // find and store max occurence and letter in hash array
         for(int i=0;i<hash.length;i++){
             if(hash[i]>max){
                 max=hash[i];
@@ -19,31 +20,30 @@ class Solution {
             }
         }
 
-        if(max> (s.length()+1)/2){
+        if(max>(s.length()+1)/2){
             return "";
         }
 
-        char result[]= new char[s.length()];
-
+        char [] answer= new char[s.length()];
         int idx=0;
-        while(hash[letter]--> 0){
-            result[idx]= (char)(letter +'a');
-            idx=idx+2;
+
+        while(hash[letter]-->0){
+            answer[idx]=(char)(letter+'a');
+            idx+=2;
         }
 
         for(int i=0;i<hash.length;i++){
             while(hash[i]-->0){
-                if(idx>=result.length){
+                if(idx>=answer.length){
                     idx=1;
                 }
-                    result[idx]=(char)(i+'a');
-                    idx+=2;
-
-                
+                answer[idx]=(char) (i+'a');
+                idx+=2;
             }
         }
 
-        return String.valueOf(result);
+        return String.valueOf(answer);
+
 
 
     }
