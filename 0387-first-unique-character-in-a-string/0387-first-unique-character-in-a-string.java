d6880ecaf7d6590c4501dbed7ae629fb27a2ef49
Jayesh Devre <84898706+JayeshDevre@users.noreply.github.com>
2025-03-12 14:32:53 -0700

    Time: 5 ms (94.1%), Space: 45.1 MB (91.89%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,14 +1,16 @@
-class Solution{
-public int firstUniqChar(String s){
-int[]freq=new int[26];
-for(char c:s.toCharArray()){
-freq[c-'a']++;
-}
-for(int i=0;i<s.length();i++){
-if(freq[s.charAt(i)-'a']==1){
-return i;
-}
-}
-return -1;
-}
+class Solution {
+    public int firstUniqChar(String s) {
+        int [] count=new int[26];
+
+        for(char c: s.toCharArray()){
+            count[c-'a']++;
+        }
+
+        for(int i=0;i<s.length();i++){
+            if(count[s.charAt(i)-'a']==1){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
