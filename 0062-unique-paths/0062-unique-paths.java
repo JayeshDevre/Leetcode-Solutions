7dc0d208c996b680e5bbc805da2ecb4dbf2f9cd0
Jayesh Devre <84898706+JayeshDevre@users.noreply.github.com>
2025-03-15 11:24:56 -0700

    Time: 0 ms (100%), Space: 40.7 MB (26.1%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,23 +1,18 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-       int[][] dp = new int[m][n];
 
-        // I initialize the first row and first column; there's only one way to reach those cells
-        for (int i = 0; i < m; i++) {
-            dp[i][0] = 1;
-        }
-        for (int j = 0; j < n; j++) {
-            dp[0][j] = 1;
-        }
+        int [][] dp= new int [m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=1;
+                }else{
+                    dp[i][j]=dp[i][j-1]+dp[i-1][j];
+                }
 
-        // I fill in the DP table using the relation: dp[i][j] = dp[i-1][j] + dp[i][j-1]
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
 
-        // I return the final value at the bottom-right corner
-        return dp[m - 1][n - 1];
+        return dp[m-1][n-1];
     }
 }
