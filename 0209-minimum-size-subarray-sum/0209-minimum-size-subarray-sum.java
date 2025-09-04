e253efda0919e9c997d95ed3a8fa47c077be2273
Jayesh Devre <84898706+JayeshDevre@users.noreply.github.com>
2025-09-04 14:15:53 -0700

    Time: 1 ms (99.51%), Space: 58.7 MB (6.48%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,21 +1,23 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int minWindowSize=Integer.MAX_VALUE;
         int low=0;
         int high=0;
-        int currentSum=0;
+        int minWindowSize=Integer.MAX_VALUE;
+        int currSum=0;
 
         while(high<nums.length){
-            currentSum+=nums[high];
+            currSum+=nums[high];
             high++;
 
-            while(currentSum>=target){
-                int currentWindowSize=high-low;
-                minWindowSize=Math.min(minWindowSize,currentWindowSize);
-                currentSum=currentSum-nums[low];
+            while(currSum>=target){
+                int currentWindow=high-low;
+                minWindowSize=Math.min(minWindowSize,currentWindow);
+                currSum-=nums[low];
                 low++;
             }
         }
+
         return minWindowSize==Integer.MAX_VALUE?0:minWindowSize;
+
     }
 }
