532b9986e72116e19f4e73c810f34c1e310bfb4e
Jayesh Devre <84898706+JayeshDevre@users.noreply.github.com>
2025-09-05 03:27:35 -0700

    Time: 27 ms (6.04%), Space: 44.7 MB (34.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,15 @@
-class Solution {
+public class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){
+        if (s.length() != t.length()) {
             return false;
         }
 
-        int arr[]=new int[26];
-
-        for(int i=0;i<s.length();i++){
-            arr[s.charAt(i)-'a']++;
-            arr[t.charAt(i)-'a']--;
+        HashMap<Character, Integer> countS = new HashMap<>();
+        HashMap<Character, Integer> countT = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i), 0) + 1);
+            countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0) + 1);
         }
-
-        for(int i:arr){
-            if(i!=0){
-                return false;
-            }
-        }
-        return true;
+        return countS.equals(countT);
     }
 }
