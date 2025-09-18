5e5f2c8857d7f7a3c54b37ad5739fe36c3741db6
Jayesh Devre <84898706+JayeshDevre@users.noreply.github.com>
2025-09-18 03:37:02 -0700

    Time: 0 ms (100%), Space: 44.5 MB (49.71%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -10,16 +10,13 @@
  * }
  */
 public class Solution {
-
-    // For this problem use fast and slow pointer method as fast pointer will move ahead by two steps and slow will move by one at some point they will meet if cycle is present.
     public boolean hasCycle(ListNode head) {
-
         ListNode fast=head;
         ListNode slow=head;
-
         while(fast!=null && fast.next!=null){
             fast=fast.next.next;
             slow=slow.next;
+
             if(fast==slow){
                 return true;
             }
