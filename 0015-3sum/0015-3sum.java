06b4226116f6bae9cd09811656a4071e827cfbcf
Jayesh Devre <84898706+JayeshDevre@users.noreply.github.com>
2025-09-30 00:00:00 -0700

    Time: 31 ms (56.7%), Space: 51.6 MB (46.53%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,20 +1,52 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> l=new ArrayList<>();
-        HashSet<List<Integer>> set=new HashSet<>();
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
-                    if(nums[i]+nums[j]+nums[k]==0){
-                        List<Integer>t= Arrays.asList(nums[i], nums[j], nums[k]);
-                        Collections.sort(t);
-                        set.add(t);
-                    }
-                }
-            }
-        }
+        // HashSet<List<Integer>> set=new HashSet<>();
+        // int n=nums.length;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         for(int k=j+1;k<n;k++){
+        //             if(nums[i]+nums[j]+nums[k]==0){
+        //                 List<Integer>t= Arrays.asList(nums[i], nums[j], nums[k]);
+        //                 Collections.sort(t);
+        //                 set.add(t);
+        //             }
+        //         }
+        //     }
+        // }
      
-        return new ArrayList<>(set);
+        // return new ArrayList<>(set);
+
+        int n=nums.length;
+        Arrays.sort(nums);
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+
+            int j=i+1;
+            int k=n-1;
+
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum==0){
+                    l.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]){
+                        j++;
+                    }
+                } else if(sum<0){
+                    j++;
+                }else{
+                    k--;
+                }
+
+                // if()
+
+            }
+
+        }
+        return l;
     }
 }
