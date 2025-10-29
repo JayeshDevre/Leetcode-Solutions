1a2ab2660ad4800b2c3102abd8f26dac2dd6b517
Jayesh Devre <84898706+JayeshDevre@users.noreply.github.com>
2025-10-28 18:17:31 -0700

    Time: 14 ms (20%), Space: 45.7 MB (26.25%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
@@ -1,32 +1,28 @@
 class Solution {
     public int bestClosingTime(String customers) {
-        int penalty = 0;
-        int minPenalty = 0;
-        int bestHour = 0;
+        int penalty=0;
+        int minPenalty=0;
+        int hour=0;
 
-        // Initially, assume shop is open all hours
-        // Count all 'Y' as penalty (since closing at hour 0 means you missed all)
-        for (char c : customers.toCharArray()) {
-            if (c == 'Y') penalty++;
+        for(Character c:customers.toCharArray()){
+            if(c=='Y'){
+                penalty++;
+            }
         }
 
-        minPenalty = penalty;
-
-        for (int i = 0; i < customers.length(); i++) {
-            if (customers.charAt(i) == 'Y') {
-                // Staying open for 'Y' removes a missed penalty
+        minPenalty=penalty;
+        for(int i=0;i<customers.length();i++){
+            if(customers.charAt(i)=='Y'){
                 penalty--;
-            } else {
-                // Staying open for 'N' adds a wasted penalty
+            }else{
                 penalty++;
             }
 
-            if (penalty < minPenalty) {
-                minPenalty = penalty;
-                bestHour = i + 1;
+            if(minPenalty>penalty){
+                minPenalty=penalty;
+                hour=i+1;
             }
         }
-
-        return bestHour;
+        return hour;
     }
 }
