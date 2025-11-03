6fc5829e4a3e6d2d2de9f4cbf1b7bd620b251951
Jayesh Devre <84898706+JayeshDevre@users.noreply.github.com>
2025-11-03 12:58:18 -0700

    Time: 11 ms (5.9%), Space: 47.4 MB (5.46%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -1,36 +1,14 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    private String preOrder(TreeNode root){
-        if(root==null){
-            return "null";
-        }   
-
-        StringBuilder sb= new StringBuilder("^");
-
-        sb.append(root.val);
-        sb.append(preOrder(root.left));
-        sb.append(preOrder(root.right));
-
-        return sb.toString();
+    private String serialize(TreeNode node) {
+        if (node == null) {
+            return "N";
+        }
+        return "(" + node.val + "," + serialize(node.left) + "," + serialize(node.right) + ")";
     }
-    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        String fulltree=preOrder(root);
-        String subtree=preOrder(subRoot);
 
-        return (fulltree.contains(subtree));
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        String rootSerialized = serialize(root);
+        String subRootSerialized = serialize(subRoot);
+        return rootSerialized.contains(subRootSerialized);
     }
 }
