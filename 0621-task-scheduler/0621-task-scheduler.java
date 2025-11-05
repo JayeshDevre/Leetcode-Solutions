e1ce595db3d04e6aaa5af3253fb30b6d0544370c
Jayesh Devre <84898706+JayeshDevre@users.noreply.github.com>
2025-11-04 20:22:08 -0700

    Time: 37 ms (15.29%), Space: 48.1 MB (5.08%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,32 +1,34 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         Map<Character, Integer> map=new HashMap<>();
-
-        for(char ch: tasks){
+//  ["A","C","A","B","D","B"], n = 1          a:2  b:2  c:1  d:1
+        for(char ch:tasks){
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
+// 2,2,1,1
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        pq.addAll(map.values());
 
-        PriorityQueue<Integer> maxHeap=new PriorityQueue<>((a,b)->b-a);
-        maxHeap.addAll(map.values());
+        int ans=0;
 
-        int time=0;
-        while(!maxHeap.isEmpty()){
-            ArrayList<Integer> list= new ArrayList<>();
-
+        while(!pq.isEmpty()){
+            List<Integer> list= new ArrayList<>();
+            // ACA   : N+1= 2
             for(int i=0;i<n+1;i++){
-                if(!maxHeap.isEmpty()){
-                list.add(maxHeap.poll());
+                if(!pq.isEmpty()){
+                    list.add(pq.poll());
                 }
             }
 
-            for(int i:list){
-                if(--i>0){
-                    maxHeap.add(i);
+            for(int freq:list){
+                if(--freq>0){
+                pq.add(freq);
                 }
             }
 
-            time+=maxHeap.isEmpty()?list.size():n+1;
+            ans+=pq.isEmpty()?list.size():n+1;
         }
-        return time;
+
+        return ans;
     }
 }
