0e5521414e117e65aee23c2de413d395d0e66246
Jayesh Devre <84898706+JayeshDevre@users.noreply.github.com>
2025-11-12 20:09:52 -0700

    Time: 15 ms (81.03%), Space: 46.4 MB (58.48%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,36 +1,21 @@
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> stack= new Stack<>();
-        int number=0;
-        char operator='+';
+        int total = 0, last = 0, number = 0;
+        char op = '+';
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch)) number = number * 10 + (ch - '0');
 
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
+            if ((!Character.isDigit(ch) && !Character.isWhitespace(ch)) || i == s.length() - 1) {
+                if (op == '+') { total += last; last = number; }
+                else if (op == '-') { total += last; last = -number; }
+                else if (op == '*') { last = last * number; }
+                else if (op == '/') { last = last / number; }
 
-            if(Character.isDigit(ch)){
-                number=(number*10)+(ch-'0');
-            }
-
-            if(!Character.isDigit(ch) && ch!=' ' || (i == s.length()-1)){
-                if(operator=='+'){
-                    stack.push(number);
-                }else if(operator=='-'){
-                    stack.push(number*-1);
-                }else if(operator=='*'){
-                    stack.push(number*stack.pop());
-                }else if(operator=='/'){
-                    stack.push(stack.pop()/number);
-                }
-
-                number=0;
-                operator=ch;
+                op = ch;
+                number = 0;
             }
         }
-        int sum=0;
-        while(!stack.isEmpty()){
-            sum=sum+stack.pop();
-        }
-        return sum;
-
+        return total + last;
     }
 }
