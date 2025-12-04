609b29244575746d91dce885197b78a36b506292
Jayesh Devre <jayeshbharat386@gmail.com>
2025-12-04 13:50:15 -0700

    Time: 133 ms (23.93%), Space: 192.5 MB (6.85%) - LeetHub

---
+++ 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countElements(int[] nums, int k) {
+        int n= nums.length;
+        Arrays.sort(nums);
+
+        Map<Integer, Integer> map= new HashMap<>();
+        for(int i=0;i<n;i++){
+            map.put(nums[i],i);
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            int last=map.get(nums[i]);
+            int greater= (n-1)-last;
+            if(greater>=k){
+                count++;
+            }
+        }
+        return count;
+
+    }
+}
