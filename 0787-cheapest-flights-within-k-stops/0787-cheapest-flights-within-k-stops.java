9138bd60a7aae2cdb201c3ea4ed8ba2bacd57c29
Jayesh Devre <jayeshbharat386@gmail.com>
2026-01-05 18:50:09 +0530

    Time: 5 ms (98.72%), Space: 46.9 MB (11.68%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,65 +1,24 @@
 class Solution {
-
-    class Pair{
-        int first;
-        int second;
-        Pair(int first, int second){
-            this.first=first;
-            this.second=second;
-        }
-    }
-
-    class Tuple{
-        int first;
-        int second;
-        int third;
-        Tuple(int f,int s,int t){
-            this.first=f;
-            this.second=s;
-            this.third=t;
-        }
-    }
-
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        ArrayList<ArrayList<Pair>> adj= new ArrayList<>();
-        int m=flights.length;
+        int [] costs= new int[n];
+        Arrays.fill(costs,Integer.MAX_VALUE);
+        costs[src]=0;
 
-        for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
-        }
+        for(int i=0;i<=k;i++){
+            int [] temp=costs.clone();
 
-        for(int i=0;i<m;i++){
-            adj.get(flights[i][0]).add(new Pair(flights[i][1],flights[i][2]));
-        }
+            for(int [] flight: flights){
+                int u=flight[0];
+                int v=flight[1];
+                int cost=flight[2];
+                if(costs[u]==Integer.MAX_VALUE) continue;
 
-        Queue<Tuple> queue= new LinkedList<>();
-        queue.offer(new Tuple(0,src,0));
-
-        int [] dist= new int[n];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[src]=0;
-
-        while(!queue.isEmpty()){
-            Tuple t=queue.poll();
-            int stops=t.first;
-            int node=t.second;
-            int cost=t.third;
-
-            if(stops>k){
-                continue;
-            }
-
-            for(Pair p: adj.get(node)){
-                int wt= p.second;
-                if(cost+wt<dist[p.first] && stops<=k){
-                    dist[p.first]=cost+wt;
-                    queue.add(new Tuple(stops+1,p.first,cost+wt));
+                if(temp[v]>costs[u]+cost){
+                    temp[v]=costs[u]+cost;
                 }
             }
+            costs=temp;
         }
-        if(dist[dst]==Integer.MAX_VALUE){
-            return -1;
-        }
-    return dist[dst];
+        return costs[dst]==Integer.MAX_VALUE?-1:costs[dst];
     }
 }
