1f319fc0b9e53666f4a13f7480734e8f98dde208
Jayesh Devre <jayeshbharat386@gmail.com>
2026-02-21 19:47:54 -0700

    Time: 29 ms (22.57%), Space: 47.1 MB (25.91%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,14 +1,19 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
-        int ans=calculate(n-1,amount, coins);
+        int [][] dp=new int[n][amount+1];
+        for(int [] row: dp){
+            Arrays.fill(row,-1);
+        }
+
+        int ans=calculate(n-1,amount, coins,dp);
         if(ans==1e9){
             return -1;
         }
         return ans;
     }
 
-    private int calculate(int index, int target, int [] coins){
+    private int calculate(int index, int target, int [] coins, int [][] dp){
         int INF=(int)1e9;
         if(index==0){
             if(target%coins[index]==0){
@@ -16,11 +21,14 @@ class Solution {
             }
             return INF;
         }
-        int notTake=0+calculate(index-1,target,coins);
+        if(dp[index][target]!=-1){
+            return dp[index][target];
+        }
+        int notTake=0+calculate(index-1,target,coins,dp);
         int take=INF;
         if(coins[index]<=target){
-            take=1+calculate(index,target-coins[index],coins);
+            take=1+calculate(index,target-coins[index],coins,dp);
         }
-        return Math.min(take, notTake);
+        return dp[index][target]=Math.min(take, notTake);
     }
 }
