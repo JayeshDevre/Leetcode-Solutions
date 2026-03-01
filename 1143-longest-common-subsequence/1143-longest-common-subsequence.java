dbd50fefe1ebe170f3fa86ab54fdc23d1d26fc14
Jayesh Devre <jayeshbharat386@gmail.com>
2026-02-28 21:21:46 -0700

    Time: 20 ms (78.11%), Space: 53.9 MB (80.99%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -3,10 +3,21 @@ class Solution {
         int m=text1.length();
         int n=text2.length();
         int [][] dp = new int[m+1][n+1];
-        for(int []d: dp){
-            Arrays.fill(d, -1);
+        // for(int []d: dp){
+        //     Arrays.fill(d, -1);
+        // }
+        // return helper(m-1, n-1, text1, text2, dp);
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+            if(text1.charAt(i-1)==text2.charAt(j-1)){
+                dp[i][j] = 1+dp[i-1][j-1];
+            }else{
+                dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+             }
         }
-        return helper(m-1, n-1, text1, text2, dp);
+        return dp[m][n];
     }
 
     private int helper(int i, int j, String s1, String s2, int [][] dp){
