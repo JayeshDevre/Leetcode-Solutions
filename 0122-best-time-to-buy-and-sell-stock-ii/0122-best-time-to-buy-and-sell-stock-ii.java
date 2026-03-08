14e03d6e758b8194dcf55e1d8221a18ce89274ab
Jayesh Devre <jayeshbharat386@gmail.com>
2026-03-07 21:25:06 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,28 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n=prices.length;
-        int [][] dp= new int[n][2];
-        for(int[] d: dp){
-            Arrays.fill(d, -1);
-        }
-        return helper(0,1,prices,n, dp);
-    }
-
-    private int helper(int ind,int buy,int[] prices, int n, int [][] dp){
-        if(ind==n){
-            return 0;
-        }
         int profit=0;
-        if(dp[ind][buy]!=-1){
-            return dp[ind][buy];
+        for(int i=1; i<prices.length; i++){
+            if (prices [i] > prices[i-1]){
+                profit+=prices[i] - prices[i-1];
+            }
         }
-        if(buy==1){
-            profit= Math.max(-prices[ind] + helper(ind+1,0 , prices, n, dp) , 0+helper(ind+1,1,prices,n, dp));
-        }else{
-            profit=Math.max(prices[ind]+helper(ind+1, 1, prices, n, dp), 0+helper(ind+1,0,prices,n, dp));
-        }
-        return dp[ind][buy]=profit;
+        return profit;
     }
-
-
 }
