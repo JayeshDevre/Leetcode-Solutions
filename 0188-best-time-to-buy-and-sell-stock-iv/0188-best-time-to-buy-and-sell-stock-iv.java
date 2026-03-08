c4ba0e5ad7f6803c16b590b7f2df6ecd1b5aed6a
Jayesh Devre <jayeshbharat386@gmail.com>
2026-03-07 23:17:14 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,27 +1,37 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
         int n= prices.length;
-        int[][]dp= new int[n][k*2];
-        for(int [] d: dp){
-            Arrays.fill(d, -1);
+        int[][]dp= new int[n+1][k*2+1];
+        // for(int [] d: dp){
+        //     Arrays.fill(d, -1);
+        // }
+        // return helper(0,0,prices,n, k, dp);
+        for( int ind= n-1; ind>=0; ind--){
+            for(int transactions=2*k-1; transactions>=0; transactions--){
+                    if(transactions%2==0){
+                        dp[ind][transactions]=Math.max(-prices[ind]+dp[ind+1][transactions+1], 0+dp[ind+1][transactions]);
+                    }else{
+                        dp[ind][transactions]= Math.max(prices[ind]+dp[ind+1][transactions+1], 0+dp[ind+1][transactions]);
+                    }
+            }
         }
-        return helper(0,0,prices,n, k, dp);
+        return dp[0][0];
     }
 
-    private int helper(int ind, int transactions, int [] prices, int n, int k, int[][] dp){
-        if(ind==n || transactions==k*2){
-            return 0;
-        }
-        int profit=0;
-        if(dp[ind][transactions]!=-1){
-            return dp[ind][transactions];
-        }
-        if(transactions%2==0){
-            profit=Math.max(-prices[ind]+helper(ind+1, transactions+1, prices, n, k, dp), 0+helper(ind+1,transactions,prices, n, k, dp));
-        }else{
-            profit= Math.max(prices[ind]+helper(ind+1, transactions+1, prices, n, k, dp), 0+helper(ind+1,transactions, prices, n, k, dp));
-        }
+    // private int helper(int ind, int transactions, int [] prices, int n, int k, int[][] dp){
+    //     if(ind==n || transactions==k*2){
+    //         return 0;
+    //     }
+    //     int profit=0;
+    //     if(dp[ind][transactions]!=-1){
+    //         return dp[ind][transactions];
+    //     }
+    //     if(transactions%2==0){
+    //         profit=Math.max(-prices[ind]+helper(ind+1, transactions+1, prices, n, k, dp), 0+helper(ind+1,transactions,prices, n, k, dp));
+    //     }else{
+    //         profit= Math.max(prices[ind]+helper(ind+1, transactions+1, prices, n, k, dp), 0+helper(ind+1,transactions, prices, n, k, dp));
+    //     }
 
-        return dp[ind][transactions]= profit;
-    }
+    //     return dp[ind][transactions]= profit;
+    // }
 }
