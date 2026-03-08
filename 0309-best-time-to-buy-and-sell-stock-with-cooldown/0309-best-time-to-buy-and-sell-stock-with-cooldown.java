9487574b0c1bbdc3cd29390a96c2130ecd1406cb
Jayesh Devre <jayeshbharat386@gmail.com>
2026-03-07 23:41:05 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,11 +1,21 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n= prices.length;
-        int [][] dp= new int[n+1][2];
-        for(int [] d: dp){
-            Arrays.fill(d, -1);
+        int [][] dp= new int[n+2][2];
+        // for(int [] d: dp){
+        //     Arrays.fill(d, -1);
+        // }
+        // return helper(0,1,prices,n,dp);
+        for(int ind=n-1; ind>=0; ind--){
+            for(int buy=0; buy<=1;buy++){
+                if(buy==1){
+                    dp[ind][buy]= Math.max(-prices[ind]+dp[ind+1][0], 0+dp[ind+1][1]);
+                }else{
+                    dp[ind][buy]= Math.max(prices[ind]+dp[ind+2][1], 0+dp[ind+1][0]);
+                }                
+            }
         }
-        return helper(0,1,prices,n,dp);
+        return dp[0][1];
     }
 
     private int helper(int ind,int buy, int[] prices, int n, int[][] dp ){
