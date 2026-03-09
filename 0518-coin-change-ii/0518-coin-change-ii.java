3c1eea9f8ef9a79e055ed6d93545697469974561
Jayesh Devre <jayeshbharat386@gmail.com>
2026-03-08 23:56:05 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,11 +1,28 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n= coins.length;
-        int[][] dp= new int[n+1][amount+1];
-        for(int [] d: dp){
-            Arrays.fill(d, -1);
+        int[][] dp= new int[n][amount+1];
+        // for(int [] d: dp){
+        //     Arrays.fill(d, -1);
+        // }
+        // return helper(n-1, amount, coins, dp);
+        for(int t=0; t<=amount ; t++){
+            if(t%coins[0]==0){
+                dp[0][t]=1;
+            }
         }
-        return helper(n-1, amount, coins, dp);
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                int notTake=dp[i-1][j];
+                int take=0;
+                if(coins[i]<=j){
+                    take= dp[i][j-coins[i]];
+                }
+                dp[i][j] = take + notTake;
+            }
+        }
+        return dp[n-1][amount];
     }
 
     private int helper(int index, int amount, int[] coins, int [][] dp){
