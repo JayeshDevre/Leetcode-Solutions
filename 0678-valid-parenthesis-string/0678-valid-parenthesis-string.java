802f1a823e97250d4138b6d104ec0598c993dccd
Jayesh Devre <jayeshbharat386@gmail.com>
2026-03-15 11:50:17 -0700

    Time: 0 ms (100%), Space: 42.7 MB (81.57%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,29 +1,34 @@
 class Solution {
     public boolean checkValidString(String s) {
-        int n= s.length();
-        Boolean [][] dp =new Boolean[n+1][n+1];
-        return solve(0,0,s,n, dp);
-    }
+        int balance=0;
 
-    private boolean solve(int index, int open, String s, int n, Boolean [][] dp){
-        if(index==n){
-            return open==0;
-        }
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c =='(' || c =='*'){
+                balance++;
+            }else{
+                balance--;
+            }
 
-        if(dp[index][open]!=null){
-            return dp[index][open];
-        }
-
-        boolean ans=false;
-        if(s.charAt(index)=='('){
-            ans = solve(index+1,open+1,s,n,dp);
-        }else if(s.charAt(index)=='*'){
-            ans = solve(index+1, open+1,s,n,dp) || solve(index+1,open,s,n,dp) || (open>0 && solve(index+1,open-1,s,n,dp));
-        }else{
-            if(open>0){
-                ans = solve(index+1,open-1,s,n,dp); 
+            if(balance<0){
+                return false;
             }
         }
-        return dp[index][open]=ans;
+
+        balance = 0; 
+
+        for(int i=s.length()-1;i>=0;i--){
+            char c = s.charAt(i);
+            if(c ==')' || c =='*'){
+                balance++;
+            }else{
+                balance--;
+            }
+
+            if(balance<0){
+                return false;
+            }
+        }
+        return true;
     }
 }
