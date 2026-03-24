9ccbfbe4e7af201be98dec1dba80576e4620587b
Jayesh Devre <jayeshbharat386@gmail.com>
2026-03-23 19:51:30 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,25 +1,32 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+        int m=grid.length;
+        int n=grid[0].length;
 
-        int[][] dp = new int[m][n];
-        dp[0][0] = grid[0][0];
-
-        for (int i = 1; i < m; i++) {
-            dp[i][0] = grid[i][0] + dp[i - 1][0];
+        int [][] dp= new int[m+1][n+1];
+        for(int [] d: dp){
+            Arrays.fill(d,-1);
         }
 
-        for (int j = 1; j < n; j++) {
-            dp[0][j] = grid[0][j] + dp[0][j - 1];
+        return helper(m-1,n-1,grid,dp);
+    }
+
+    private int helper(int i, int j, int [][] grid, int [][] dp){
+        if(i==0 && j==0){
+            return grid[i][j];
         }
 
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
-            }
+        if(i<0 || j<0){
+            return (int)1e9;
         }
 
-        return dp[m - 1][n - 1];
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        int up=grid[i][j]+ helper(i-1,j,grid,dp);
+        int left=grid[i][j]+helper(i,j-1,grid,dp);
+
+        return dp[i][j]=Math.min(up,left);
     }
 }
