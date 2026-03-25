5db2798b7b8143794e60c6352acb53d670654885
Jayesh Devre <jayeshbharat386@gmail.com>
2026-03-24 22:51:20 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,47 +1,31 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int n= coins.length;
-        int[][] dp= new int[n][amount+1];
-        // for(int [] d: dp){
-        //     Arrays.fill(d, -1);
-        // }
-        // return helper(n-1, amount, coins, dp);
-        for(int t=0; t<=amount ; t++){
-            if(t%coins[0]==0){
-                dp[0][t]=1;
-            }
+        int n=coins.length;
+        int dp[][] = new int[n][amount+1];
+        for(int [] d: dp){
+            Arrays.fill(d,-1);
         }
-
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=amount;j++){
-                int notTake=dp[i-1][j];
-                int take=0;
-                if(coins[i]<=j){
-                    take= dp[i][j-coins[i]];
-                }
-                dp[i][j] = take + notTake;
-            }
-        }
-        return dp[n-1][amount];
+        return helper(n-1, amount, coins,dp);
     }
 
-    private int helper(int index, int amount, int[] coins, int [][] dp){
+    private int helper(int index, int amount, int[] coins, int[][] dp){
         if(index==0){
             if(amount%coins[0]==0){
                 return 1;
             }
             return 0;
         }
+
         if(dp[index][amount]!=-1){
             return dp[index][amount];
         }
 
-        int notTake=helper(index-1, amount, coins, dp);
-        int take=0;
+        int notPick= helper(index-1, amount, coins,dp);
+        int pick=0;
         if(coins[index]<=amount){
-            take= helper(index, amount-coins[index], coins, dp);
+            pick=helper(index, amount-coins[index],coins,dp);
         }
 
-        return dp[index][amount]= take+notTake;
+        return dp[index][amount]=pick + notPick;
     }
 }
