1bc0427eae189472924b07fa0b95326e0f41bdf4
Jayesh Devre <jayeshbharat386@gmail.com>
2026-03-26 00:14:34 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,16 +1,17 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int maxSum=Integer.MIN_VALUE;
         int sum=0;
 
+        int maxSum=Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
             if(sum<0){
                 sum=0;
             }
 
-            sum=sum+nums[i];
-            maxSum=Math.max(maxSum, sum);
+            maxSum=Math.max(sum,maxSum);
         }
+
         return maxSum;
     }
 }
