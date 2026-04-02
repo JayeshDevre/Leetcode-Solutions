0cb4afee4d31fc573f1a50b64769de1e599ab341
Jayesh Devre <jayeshbharat386@gmail.com>
2026-04-02 13:11:45 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,22 +1,39 @@
 class Solution {
     public int trap(int[] height) {
       int n=height.length;
-      int [] prefix= new int[n];
-      int [] suffix= new int[n];
+      int left=0;
+      int leftMax=height[left];
+      int right=n-1;
+      int rightMax=height[right];
       int water=0;
-      prefix[0]=height[0];
-      suffix[n-1]= height[n-1];
 
-      for(int i=1;i<n;i++){
-        prefix[i]=Math.max(prefix[i-1],height[i]);
+      while(left<right){
+        if(leftMax<rightMax){
+            left++;
+            leftMax=Math.max(height[left],leftMax);
+            water+=leftMax-height[left];
+        }else{
+            right--;
+            rightMax=Math.max(rightMax,height[right]);
+            water+=rightMax-height[right];
+        }
       }
-      for(int i=n-2;i>=0;i--){
-        suffix[i]=Math.max(suffix[i+1],height[i]);
-      }
+    //   int [] prefix= new int[n];
+    //   int [] suffix= new int[n];
+    //   int water=0;
+    //   prefix[0]=height[0];
+    //   suffix[n-1]= height[n-1];
 
-      for(int i=0;i<n;i++){
-        water+= Math.min(prefix[i],suffix[i])-height[i];
-      }
-      return water;
+    //   for(int i=1;i<n;i++){
+    //     prefix[i]=Math.max(prefix[i-1],height[i]);
+    //   }
+    //   for(int i=n-2;i>=0;i--){
+    //     suffix[i]=Math.max(suffix[i+1],height[i]);
+    //   }
+
+    //   for(int i=0;i<n;i++){
+    //     water+= Math.min(prefix[i],suffix[i])-height[i];
+    //   }
+        return water;
     }
 }
