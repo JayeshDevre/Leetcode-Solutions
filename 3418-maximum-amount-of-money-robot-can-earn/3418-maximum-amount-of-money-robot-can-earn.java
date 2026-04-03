7f189f90eadc0b5418ac3291448b759d891c34cb
Jayesh Devre <jayeshbharat386@gmail.com>
2026-04-03 04:02:31 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.java
+++ 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.java
@@ -1,41 +1,32 @@
 class Solution {
     int m,n;
-    Integer[][][] dp;
     public int maximumAmount(int[][] coins) {
-        
-        n=coins.length;
-        m=coins[0].length;
-        dp = new Integer[n][m][3];
-        return calculate(coins, 0, 0, 2);
+        m=coins.length;
+        n=coins[0].length;
+        return helper(coins, 0,0, 2);
     }
 
-    private int calculate(int [][] coins, int i, int j, int neu){
-        if(i==n-1 && j==m-1){
+    private int helper(int [][] coins , int i, int j, int neu){
+        if(i==m-1 && j==n-1){
             if(coins[i][j]<0 && neu>0){
                 return 0;
             }
             return coins[i][j];
         }
 
-        if(i>=n || j>=m){
+        if(i>=m || j>=n){
             return Integer.MIN_VALUE;
         }
 
-        if(dp[i][j][neu]!=null){
-            return dp[i][j][neu];
-        }
-
-        int take = coins[i][j]+ Math.max(calculate(coins, i+1,j,neu),calculate(coins, i, j+1, neu));
+        int take=coins[i][j]+ Math.max(helper(coins,i+1,j,neu), helper(coins,i,j+1,neu));
 
         int skip=Integer.MIN_VALUE;
         if(coins[i][j]<0 && neu>0){
-            int skipDown=calculate(coins,i+1,j ,neu-1);
-            int skipRight = calculate(coins,i,j+1,neu-1);
-
-            skip=Math.max(skipDown, skipRight);
+            int skipdown=helper(coins,i+1,j,neu-1);
+            int skipright=helper(coins,i,j+1,neu-1);
+            skip=Math.max(skipdown, skipright);
         }
 
-        return dp[i][j][neu] = Math.max(take,skip);
-
+        return Math.max(skip,take);
     }
 }
