714c599a67517e1c828a380a2657c58e67a76bfb
Jayesh Devre <jayeshbharat386@gmail.com>
2026-04-10 16:15:56 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,22 +1,22 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        int left=0;
-        int ans=0;
-        int maxf=0;
+       HashMap<Character, Integer> map= new HashMap<>();
+       int maxF=0;
+       int ans=0;
+       int left=0;
+       
 
-        for(int right=0; right<s.length(); right++){
-            char c= s.charAt(right);
-            map.put(c, map.getOrDefault(c,0)+1);
-            maxf= Math.max(maxf, map.get(c));
+       for(int right=0;right<s.length();right++){
+        char c=s.charAt(right);
+        map.put(c, map.getOrDefault(c,0)+1);
+        maxF = Math.max(maxF, map.get(c));
 
-            if((right-left+1)-maxf > k){
-                map.put(s.charAt(left), map.getOrDefault(s.charAt(left),0)-1);
-                left++;
-            }
-
-            ans=Math.max(ans, right-left+1);
+        if((right-left+1) - maxF > k ){
+            map.put(s.charAt(left), map.get(s.charAt(left))-1);
+            left++;
         }
-        return ans;
+        ans=Math.max(ans, right-left+1);
+       }
+       return ans;
     }
 }
