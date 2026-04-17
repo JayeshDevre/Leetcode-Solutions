5dab0640690f44c8e70698541a424a7b4cd716a3
Jayesh Devre <jayeshbharat386@gmail.com>
2026-04-16 18:10:13 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -19,10 +19,12 @@ class Solution {
             return false;
         }
 
-        if(root.val==targetSum && root.left==null && root.right== null){
+        if(root.val==targetSum && root.left==null && root.right==null){
             return true;
         }
+        
 
-        return hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val) ;
+        return hasPathSum(root.left, targetSum-root.val) || hasPathSum(root.right, targetSum-root.val);
     }
+
 }
