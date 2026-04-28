b8bca82d51e10649f3d4981c11463c4933e4b1bc
Jayesh Devre <jayeshbharat386@gmail.com>
2026-04-27 18:18:21 -0700

    Time: 0 ms (100%), Space: 43 MB (46.24%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -18,12 +18,10 @@ class Solution {
         if(root==null){
             return null;
         }
-
         TreeNode left=invertTree(root.left);
         TreeNode right=invertTree(root.right);
-
-        root.right=left;
         root.left=right;
+        root.right=left;
         return root;
     }
 }
