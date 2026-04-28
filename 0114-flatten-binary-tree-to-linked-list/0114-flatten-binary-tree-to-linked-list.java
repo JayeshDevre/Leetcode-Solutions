eeab9f7d2b9cda0795e4062d10a14775085c88c9
Jayesh Devre <jayeshbharat386@gmail.com>
2026-04-27 20:21:51 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -14,19 +14,13 @@
  * }
  */
 class Solution {
+    TreeNode prev=null;
     public void flatten(TreeNode root) {
-        TreeNode current=root;
-        while(current!=null){
-            if(current.left!=null){
-                TreeNode temp=current.left;
-                while(temp.right!=null){
-                    temp=temp.right;
-                }
-                temp.right=current.right;
-                current.right=current.left;
-                current.left=null;
-            }
-            current=current.right;
-        }
+        flatten(root.right);
+        flatten(root.left);
+        root.right=prev;
+        root.left=null;
+
+        prev=root;
     }
 }
