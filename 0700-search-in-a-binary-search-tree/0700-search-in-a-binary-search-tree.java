289217641c401293ed995fffe530d906ef3d513e
Jayesh Devre <jayeshbharat386@gmail.com>
2026-05-05 19:45:07 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -15,19 +15,15 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if(root ==null){
+        if(root==null){
             return null;
         }
-
         if(root.val==val){
             return root;
+        }else if(root.val>val){
+            return searchBST(root.left,val);
+        }else{
+            return searchBST(root.right,val);
         }
-
-        TreeNode n=searchBST(root.left,val);
-        if(n!=null){
-            return n;
-        }
-
-        return searchBST(root.right,val);
     }
 }
