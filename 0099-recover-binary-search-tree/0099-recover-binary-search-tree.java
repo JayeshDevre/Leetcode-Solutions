1e7d75a688d7ebd444a59e43fca5da81db5da5d1
Jayesh Devre <jayeshbharat386@gmail.com>
2026-05-07 12:10:34 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -14,10 +14,25 @@
  * }
  */
 class Solution {
-    private TreeNode first;
     private TreeNode prev;
+    private TreeNode first;
     private TreeNode middle;
     private TreeNode last;
+
+    public void recoverTree(TreeNode root) {
+        first=middle=last=null;
+        prev=new TreeNode(Integer.MIN_VALUE);
+        inOrder(root);
+        if(first!=null && last!=null){
+            int temp=first.val;
+            first.val=last.val;
+            last.val=temp;
+        }else{
+            int temp=first.val;
+            first.val=middle.val;
+            middle.val=temp;
+        }
+    }
 
     private void inOrder(TreeNode root){
         if(root==null){
@@ -25,30 +40,16 @@ class Solution {
         }
 
         inOrder(root.left);
-        if(prev!=null && (root.val<prev.val)){            
-            if(first==null){       
-               first=prev;
-               middle=root;
-           }else{
-               last=root;
-           }
+
+        if(prev!=null && (root.val<prev.val)){
+            if(first==null){
+                first=prev;
+                middle=root;
+            }else{
+                last=root;
+            }
         }
         prev=root;
         inOrder(root.right);
-    }
-
-    public void recoverTree(TreeNode root) {
-        first=middle=last=null;
-        prev=new TreeNode(Integer.MIN_VALUE);  
-        inOrder(root);
-        if(first!=null && last!=null){
-            int temp=first.val;
-            first.val=last.val;
-            last.val=temp;
-        }else if(first!=null && middle!=null){
-            int temp=first.val;
-            first.val=middle.val;
-            middle.val=temp;           
-        }
     }
 }
